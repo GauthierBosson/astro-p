419d106e7b9f8387f28ec9cc3ea457b5ee829aa4
Gauthier Bosson <bosson.gauthier27@gmail.com>
2023-10-09 20:24:47 +0200

    📦 chore(package.json): add @fontsource/krub and @fontsource/rubik as dependencies to support custom fonts in the project
    🆕 feat(BaseLayout.astro): create a new BaseLayout component to serve as the base layout for all pages
    
    🆕 feat(index.astro): import @fontsource/krub and @fontsource/rubik to use custom fonts in the index page, update the styling of the heading to improve readability
    
    🔧 chore(tailwind.config.cjs): extend the default font family with Krub and Rubik fonts, update the drop shadow values for better visual effect

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,18 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				sans: ['Krub', 'Rubik', ...defaultTheme.fontFamily.sans],
+			},
 			dropShadow: {
 				'4xl': [
-					'0 0px 65px rgba(0, 0, 255, 0.6)',
-					'0 10px 65px rgba(255, 102, 0, 0.6)',
-					'0 20px 65px rgba(163, 77, 255, 0.3)'
+					'0 -5px 42px rgba(0, 0, 255, 0.35)',
+					'0 0px 42px rgba(255, 102, 0, 0.35)',
+					'0 5px 42px rgba(163, 77, 255, 0.35)'
 				]
 			},
 			animation: {
